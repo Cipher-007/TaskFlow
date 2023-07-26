8003440f830071f40430a3b63a0850eee1a421ab
Cipher-007 <gaurav7deshmukh@gmail.com>
2023-07-27 02:01:45 +0530

    added auth

---
+++ lib/auth.ts
@@ -0,0 +1,49 @@
+import bcrypt from "bcrypt";
+import { SignJWT, jwtVerify } from "jose";
+import { db } from "./db";
+
+export function hashPassword(password: string) {
+  bcrypt.hash(password, 10);
+}
+
+export function comparePasswords(
+  plainTextPassword: string,
+  hashedPassword: string
+) {
+  bcrypt.compare(plainTextPassword, hashedPassword);
+}
+
+export function createJSW(user) {
+  const iat = Math.floor(Date.now() / 1000);
+  const exp = iat + 60 * 60 * 24 * 7;
+
+  return new SignJWT({ payload: { id: user.id, email: user.email } })
+    .setProtectedHeader({ alg: "HS256", typ: "JWT" })
+    .setExpirationTime(exp)
+    .setIssuedAt(iat)
+    .setNotBefore(iat)
+    .sign(new TextEncoder().encode(process.env.JWT_SECRET));
+}
+
+export async function validateJWT(jwt) {
+  const { payload } = await jwtVerify(
+    jwt,
+    new TextEncoder().encode(process.env.JWT_SECRET)
+  );
+
+  return payload.payload as any;
+}
+
+export async function getUserFromCookie(cookies) {
+  const jwt = cookies.get(process.env.COOKIE_NAME);
+
+  const { id } = await validateJWT(jwt.value);
+
+  const user = await db.user.findUnique({
+    where: {
+      id: id as string,
+    },
+  });
+
+  return user;
+}
