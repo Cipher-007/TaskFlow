6de70a901aede5734e4dad91139cbffcadc5e088
Cipher-007 <gaurav7deshmukh@gmail.com>
2023-07-28 02:34:27 +0530

    created projects endpoint

---
+++ app/api/projects/route.ts
@@ -0,0 +1,32 @@
+import { validateJWT } from "@/lib/auth";
+import { db } from "@/lib/db";
+import { cookies } from "next/headers";
+
+export async function POST(request: Request) {
+  const data: { name: string; description: string; due: Date } =
+    await request.json();
+
+  const cookieStore = cookies();
+
+  const c = cookieStore.get(process.env.COOKIE_NAME! as string);
+
+  if (!c) {
+    return new Response("Unauthorized");
+  }
+
+  const user = await validateJWT(c.value);
+
+  await db.project.create({
+    // /* @ts-ignore */
+    data: {
+      name: data.name,
+      description: data.description,
+      owner: { connect: { id: user.id } },
+      due: data.due,
+    },
+  });
+
+  return new Response("Project created", {
+    status: 201,
+  });
+}
