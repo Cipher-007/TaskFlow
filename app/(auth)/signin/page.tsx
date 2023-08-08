4037c84aff4e6edc416ee2cbd5408cde8f118607
Cipher-007 <gaurav7deshmukh@gmail.com>
2023-08-08 10:02:07 +0530

    changed signin page

--- app/(auth)/signin/page.tsx
+++ app/(auth)/signin/page.tsx
@@ -1,9 +1,34 @@
-import AuthForm from "@/components/Authform";
+import SigninForm from "@/components/SigninForm";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import Link from "next/link";
+
+export const metadata = {
+  title: "Sign In",
+  description: "Sign In Page",
+};
 
 export default function Signin() {
   return (
-    <div>
-      <AuthForm mode="signin" />
-    </div>
+    <Card className="w-[500px]">
+      <CardHeader>
+        <CardTitle>Sign In</CardTitle>
+        <CardDescription>Sign in your account in one-click</CardDescription>
+      </CardHeader>
+      <CardContent className="pb-0">
+        <SigninForm />
+      </CardContent>
+      <CardFooter>
+        <Link href="/register" className="font-bold text-blue-600">
+          Don&apos;t have an account?
+        </Link>
+      </CardFooter>
+    </Card>
   );
 }
