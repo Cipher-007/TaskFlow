44e2ed61466753e9e8860c8a901ec61e45c5f8ea
Cipher-007 <gaurav7deshmukh@gmail.com>
2023-08-08 17:38:18 +0530

    updated component to use shadcn/ui card

--- components/Greetings.tsx
+++ components/Greetings.tsx
@@ -1,8 +1,14 @@
 import { getUserFromCookie } from "@/lib/auth";
 import { cookies } from "next/headers";
-import ButtonC from "./Button";
-import Card from "./Card";
 import { RequestCookies } from "next/dist/compiled/@edge-runtime/cookies";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "./ui/card";
+import { Button } from "./ui/button";
 
 async function getData() {
   return await getUserFromCookie(cookies() as unknown as RequestCookies);
@@ -12,18 +18,20 @@ export default async function Greetings() {
   const user = await getData();
 
   return (
-    <Card className="relative w-full py-4">
-      <div className="mb-4">
-        <h1 className="mb-4 text-3xl font-bold text-gray-700">
-          Hello, {user!.firstName}!
-        </h1>
+    <Card className="relative w-full rounded-2xl">
+      <CardHeader>
+        <CardTitle>Hello, {user!.firstName}!</CardTitle>
+      </CardHeader>
+      <CardContent>
         <h4 className="text-xl text-gray-400">
           Check your daily tasks and schedule
         </h4>
-      </div>
-      <div>
-        <ButtonC size="large">Today&apos;s Schedule</ButtonC>
-      </div>
+      </CardContent>
+      <CardFooter>
+        <Button className="rounded-3xl bg-violet-500 px-8 py-4 text-2xl font-bold transition duration-200 ease-in-out hover:scale-110 hover:bg-violet-600 active:scale-100">
+          Today&apos;s Schedule
+        </Button>
+      </CardFooter>
     </Card>
   );
 }
