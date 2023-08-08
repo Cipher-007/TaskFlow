ed324078190ea7801898057104f07e6a0602d4a9
Cipher-007 <gaurav7deshmukh@gmail.com>
2023-08-08 10:01:44 +0530

    changed register page

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -1,9 +1,34 @@
-import AuthForm from "@/components/Authform";
+import RegisterForm from "@/components/RegisterForm";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import Link from "next/link";
+
+export const metadata = {
+  title: "Register",
+  description: "Register Page",
+};
 
 export default function Register() {
   return (
-    <div>
-      <AuthForm mode="register" />
-    </div>
+    <Card className="w-[500px]">
+      <CardHeader>
+        <CardTitle>Create Account</CardTitle>
+        <CardDescription>Create your new account in one-click</CardDescription>
+      </CardHeader>
+      <CardContent className="pb-0">
+        <RegisterForm />
+      </CardContent>
+      <CardFooter>
+        <Link href="/signin" className="font-bold text-blue-600">
+          Already have an account?
+        </Link>
+      </CardFooter>
+    </Card>
   );
 }
