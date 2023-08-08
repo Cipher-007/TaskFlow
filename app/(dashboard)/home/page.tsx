ec445db26c5f6e3d9208ab603745233d5d4b63e2
Cipher-007 <gaurav7deshmukh@gmail.com>
2023-08-08 18:12:05 +0530

    added no project case

--- app/(dashboard)/home/page.tsx
+++ app/(dashboard)/home/page.tsx
@@ -1,8 +1,8 @@
 import Greetings from "@/components/Greetings";
 import GreetingsSkeleton from "@/components/GreetingsSkeleton";
-import NewProject from "@/components/NewProject";
-import ProjectCard from "@/components/ProjectCard";
-import TaskCard from "@/components/TaskCard";
+import NewProject from "@/components/Project/NewProject";
+import ProjectCard from "@/components/Project/ProjectCard";
+import TaskCards from "@/components/Task/TaskCards";
 import { getUserFromCookie } from "@/lib/auth";
 import { db } from "@/lib/db";
 import { RequestCookies } from "next/dist/compiled/@edge-runtime/cookies";
@@ -10,9 +10,7 @@ import { cookies } from "next/headers";
 import Link from "next/link";
 import { Suspense } from "react";
 
-import { Metadata } from "next";
-
-export const metadata: Metadata = {
+export const metadata = {
   title: "Home",
   description: "Home Page",
 };
@@ -39,20 +37,26 @@ export default async function Page() {
           </Suspense>
         </div>
         <div className="flex-2 -m-3 mt-3 flex grow flex-wrap items-center ">
-          {projects.map((project) => (
-            <div key={project.id} className="w-1/3 p-3">
-              <Link href={`/project/${project.id}`}>
-                <ProjectCard project={project} />
-              </Link>
+          {projects.length > 0 ? (
+            projects.map((project) => (
+              <div key={project.id} className="w-1/3 p-3">
+                <Link href={`/project/${project.id}`}>
+                  <ProjectCard project={project} />
+                </Link>
+              </div>
+            ))
+          ) : (
+            <div className="w-full text-center text-3xl font-medium">
+              <p>No projects yet</p>
             </div>
-          ))}
+          )}
           <div className="w-1/3 p-4">
             <NewProject />
           </div>
         </div>
         <div className="flex-2 mt-6 flex w-full grow">
           <div className="w-full pb-4">
-            <TaskCard />
+            <TaskCards />
           </div>
         </div>
       </div>
