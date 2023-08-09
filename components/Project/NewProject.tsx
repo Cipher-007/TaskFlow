00c2ddcfed79016899c7987bf964fac09511512f
Cipher-007 <gaurav7deshmukh@gmail.com>
2023-08-10 02:06:54 +0530

    using shadcn/ui Dialog

--- components/Project/NewProject.tsx
+++ components/Project/NewProject.tsx
@@ -1,97 +1,201 @@
 "use client";
-import { createNewProject } from "@/lib/api";
-import { FormEvent, useState } from "react";
-import Modal from "react-modal";
-import ButtonC from "../Button";
-import { Input } from "@/components/ui/input";
-
-import { format } from "date-fns";
-import { Calendar as CalendarIcon } from "lucide-react";
-
-import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import { Calendar } from "@/components/ui/calendar";
+import { Input } from "@/components/ui/input";
 import {
   Popover,
   PopoverContent,
   PopoverTrigger,
 } from "@/components/ui/popover";
+import { createNewProject } from "@/lib/api";
+import { cn } from "@/lib/utils";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { format } from "date-fns";
+import { Calendar as CalendarIcon, Plus } from "lucide-react";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "../ui/dialog";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
+import { Textarea } from "../ui/textarea";
+import { useToast } from "../ui/use-toast";
+import { useRouter } from "next/navigation";
 
-Modal.setAppElement("#modal");
+const projectFormSchema = z.object({
+  name: z
+    .string()
+    .min(2, {
+      message: "Project name must be at least 2 characters.",
+    })
+    .max(15, {
+      message: "Project name must not be longer than 30 characters.",
+    }),
+  description: z
+    .string()
+    .min(2, {
+      message: "Project description must be at least 2 characters.",
+    })
+    .max(255, {
+      message: "Project description must not be longer than 100 characters.",
+    }),
+  due: z.coerce.date(),
+});
+
+type ProjectFormValue = z.infer<typeof projectFormSchema>;
 
 export default function NewProject() {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [name, setName] = useState("");
-  const [description, setDescription] = useState("");
-  const [date, setDate] = useState<Date>();
+  let defaultValues: Partial<ProjectFormValue> = {
+    name: "",
+    description: "",
+  };
 
-  const openModal = () => setIsModalOpen(true);
-  const closeModal = () => setIsModalOpen(false);
+  const { toast } = useToast();
 
-  async function submitHandler(e: FormEvent<HTMLFormElement>) {
-    e.preventDefault();
+  const router = useRouter();
 
-    const due = date!.toISOString();
+  const [open, setOpen] = useState(false);
+  const [isDisabled, setIsDisabled] = useState(false);
 
-    const project = { name, description, due };
+  const form = useForm<ProjectFormValue>({
+    resolver: zodResolver(projectFormSchema),
+    defaultValues,
+    mode: "onChange",
+  });
 
-    await createNewProject(project);
+  async function onSubmit(data: ProjectFormValue) {
+    setIsDisabled(true);
+    const body = { ...data, due: data.due.toISOString() };
 
-    closeModal();
+    const res = await createNewProject(body);
+
+    if (res.ok) {
+      toast({
+        title: "Project created",
+        description: "Your project has been created.",
+      });
+      setOpen(false);
+      form.resetField("name");
+      form.resetField("description");
+      form.resetField("due");
+      router.refresh();
+    }
+    setIsDisabled(false);
   }
 
   return (
     <div className="flex items-center justify-center px-6 py-8 transition-all duration-200 ease-in-out hover:scale-105">
-      <ButtonC onClick={() => openModal()}>+ New Project</ButtonC>
-
-      <Modal
-        isOpen={isModalOpen}
-        onRequestClose={closeModal}
-        overlayClassName="bg-[rgba(0,0,0,.4)] flex justify-center items-center absolute top-0 left-0 h-screen w-screen"
-        className="w-1/2 rounded-xl bg-white p-8"
-      >
-        <h1 className="mb-6 text-3xl">New Project</h1>
-        <form
-          className="flex flex-col items-center gap-1"
-          onSubmit={submitHandler}
-        >
-          <Input
-            className="mb-4"
-            placeholder="Project name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-          <Input
-            className="mb-4"
-            placeholder="Description"
-            value={description}
-            onChange={(e) => setDescription(e.target.value)}
-          />
-          <Popover>
-            <PopoverTrigger asChild>
-              <Button
-                variant={"outline"}
-                className={cn(
-                  "w-[280px] justify-start text-left font-normal",
-                  !date && "text-muted-foreground",
-                )}
-              >
-                <CalendarIcon className="mr-2 h-4 w-4" />
-                {date ? format(date, "PPP") : <span>Pick due date</span>}
-              </Button>
-            </PopoverTrigger>
-            <PopoverContent className="w-auto p-0">
-              <Calendar
-                mode="single"
-                selected={date}
-                onSelect={setDate}
-                initialFocus
-              />
-            </PopoverContent>
-          </Popover>
-          <Button type="submit">Create</Button>
-        </form>
-      </Modal>
+      <Dialog open={open} onOpenChange={setOpen}>
+        <DialogTrigger asChild>
+          <Button>
+            <Plus className="mr-1" />
+            New Project
+          </Button>
+        </DialogTrigger>
+        <DialogContent className="sm:max-w-[425px]">
+          <DialogHeader>
+            <DialogTitle>New Project</DialogTitle>
+            <DialogDescription>
+              Fill out the details below to initiate a new project
+            </DialogDescription>
+          </DialogHeader>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)}>
+              <div className="grid gap-4 py-4">
+                <FormField
+                  control={form.control}
+                  name="name"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Project Name</FormLabel>
+                      <FormControl>
+                        <Input placeholder="Project 0" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="description"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Description</FormLabel>
+                      <FormControl>
+                        <Textarea
+                          placeholder="Project 0 description"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="due"
+                  render={({ field }) => (
+                    <FormItem className="grid grid-cols-4 items-center gap-4">
+                      <FormLabel>Due Date</FormLabel>
+                      <div className="col-span-3">
+                        <Popover>
+                          <PopoverTrigger asChild>
+                            <FormControl>
+                              <Button
+                                variant={"outline"}
+                                className={cn(
+                                  "w-[280px] justify-start text-left font-normal",
+                                  !field.value && "text-muted-foreground",
+                                )}
+                              >
+                                <CalendarIcon className="mr-2 h-4 w-4" />
+                                {field.value ? (
+                                  format(field.value, "PPP")
+                                ) : (
+                                  <span>Pick due date</span>
+                                )}
+                              </Button>
+                            </FormControl>
+                          </PopoverTrigger>
+                          <PopoverContent>
+                            <Calendar
+                              mode="single"
+                              selected={field.value}
+                              onSelect={field.onChange}
+                              disabled={(data) => data > new Date("2025-01-01")}
+                              initialFocus
+                            />
+                          </PopoverContent>
+                        </Popover>
+                      </div>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+              <DialogFooter>
+                <Button type="submit" disabled={isDisabled}>
+                  Create Project
+                </Button>
+              </DialogFooter>
+            </form>
+          </Form>
+        </DialogContent>
+      </Dialog>
     </div>
   );
 }
