52ebb90bd196ff0b687cea86317996bd2b0c5021
Cipher-007 <gaurav7deshmukh@gmail.com>
2023-08-10 14:36:15 +0530

    updated with shadcn/ui card

--- components/Task/TaskSkeleton.tsx
+++ components/Task/TaskSkeleton.tsx
@@ -1,14 +1,27 @@
-import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "../ui/card";
 
 export default function TaskSkeleton() {
   return (
-    <Card className="m-5 py-2">
-      <CardHeader>
-        <CardTitle className="h-8 w-40 animate-pulse rounded bg-gray-300"></CardTitle>
+    <Card className="m-5 flex flex-row py-2">
+      <CardHeader className="basis-1/3">
+        <CardTitle className="h-6 w-28 animate-pulse rounded bg-gray-300"></CardTitle>
+        <CardDescription className="h-5 w-52 animate-pulse rounded bg-gray-300"></CardDescription>
       </CardHeader>
-      <CardContent>
-        <div className="h-4 w-1/4 animate-pulse rounded bg-gray-300"></div>
+      <CardContent className="flex basis-1/3 animate-pulse flex-col items-center px-1 py-6">
+        <div className="mb-2 h-5 w-36 bg-gray-300" />
+        <div className="h-6 w-32 rounded-lg bg-gray-300" />
       </CardContent>
+      <CardFooter className="flex basis-1/3 animate-pulse justify-end gap-4 py-6 pr-14">
+        <div className="h-10 w-[90px] rounded-md bg-gray-300" />
+        <div className="h-10 w-[90px] rounded-md bg-gray-300" />
+      </CardFooter>
     </Card>
   );
 }
