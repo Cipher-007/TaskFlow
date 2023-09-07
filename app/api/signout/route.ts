bd451f6e0c9147cd245864a7d341d6fa09869f3e
Cipher-007 <gaurav7deshmukh@gmail.com>
2023-09-07 14:45:22 +0530

    remove cookie

--- app/api/signout/route.ts
+++ app/api/signout/route.ts
@@ -1,8 +1,5 @@
+import { cookies } from "next/headers";
+
 export async function POST() {
-  return new Response(null, {
-    status: 200,
-    headers: {
-      "Set-Cookie": `${process.env.COOKIE_NAME}=; Max-Age=0; Expires=Thu, 01 Jan 1970 00:00:00 GMT; Path=/;`,
-    },
-  });
+  cookies().delete(process.env.COOKIE_NAME!);
 }
