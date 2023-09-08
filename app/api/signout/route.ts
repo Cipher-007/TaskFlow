5c89075f3fef3ee4d3978cfa362f29cbc95ade8a
Cipher-007 <gaurav7deshmukh@gmail.com>
2023-09-08 23:16:56 +0530

    reverted to last commit

--- app/api/signout/route.ts
+++ app/api/signout/route.ts
@@ -1,5 +1,8 @@
-import { cookies } from "next/headers";
-
 export async function POST() {
-  cookies().delete(process.env.COOKIE_NAME!);
+  return new Response(null, {
+    status: 200,
+    headers: {
+      "Set-Cookie": `${process.env.COOKIE_NAME}=; Max-Age=0; Expires=Thu, 01 Jan 1970 00:00:00 GMT; Path=/;`,
+    },
+  });
 }
