885d22b91f1da156c1d02ac13e2cd47c712cc742
Cipher-007 <gaurav7deshmukh@gmail.com>
2023-09-09 14:55:58 +0530

    shifted logic to Profile component

--- app/(dashboard)/settings/page.tsx
+++ app/(dashboard)/settings/page.tsx
@@ -1,24 +1,11 @@
-import ProfileForm from "@/components/ProfileForm";
-import { getUserFromCookie } from "@/lib/auth";
-import { db } from "@/lib/db";
+import Profile from "@/components/Profile/Profile";
 import { Metadata } from "next";
-import { cookies } from "next/headers";
 
 export const metadata: Metadata = {
   title: "Settings",
   description: "Edit Profile",
 };
 
-async function getData() {
-  const user = await getUserFromCookie(cookies());
-  return await db.user.findUnique({
-    where: {
-      id: user?.id,
-    },
-  });
-}
-
-export default async function Settings() {
-  const user = await getData();
-  return <ProfileForm data={user!} />;
+export default function Settings() {
+  return <Profile />;
 }
