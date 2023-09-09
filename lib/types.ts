6aa992724bd6f4d75920ea367633ebecdd713baf
Cipher-007 <gaurav7deshmukh@gmail.com>
2023-09-09 14:57:12 +0530

    new types

--- lib/types.ts
+++ lib/types.ts
@@ -1,3 +1,12 @@
+import type { Task } from "@prisma/client";
+
+export type TaskFormProp = {
+  mode: "create" | "edit";
+  task?: Task;
+  content?: { title: string; toastDescription: string };
+  setToggle?: (toggle: boolean) => void;
+};
+
 export enum TASK_STATUS {
   NOT_STARTED = "NOT_STARTED",
   STARTED = "STARTED",
