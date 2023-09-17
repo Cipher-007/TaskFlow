343396b46d973e6fc4d5f48046ff0a6935b77948
Cipher-007 <gaurav7deshmukh@gmail.com>
2023-09-17 15:17:01 +0530

    removed unused type and imports

--- app/(dashboard)/project/[id]/loading.tsx
+++ app/(dashboard)/project/[id]/loading.tsx
@@ -1,9 +1,6 @@
 import TasksCardSkeleton from "@/components/Task/TasksCardSkeleton";
-import React from "react";
 
-type Props = {};
-
-export default function ProjectLoader({}: Props) {
+export default function ProjectLoading() {
   return (
     <div className="mx-32 w-full">
       <TasksCardSkeleton title="none" />
