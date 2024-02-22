c1715bd454cbc7065374d9052c5a2f9f4555b6ed
Gaurav Deshmukh <gaurav7deshmukh@gmail.com>
2024-02-22 21:47:32 +0530

    switched to http connection

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,10 +1,17 @@
-import { neonConfig, Pool } from "@neondatabase/serverless";
-import { drizzle } from "drizzle-orm/neon-serverless";
+// import { neonConfig, Pool } from "@neondatabase/serverless";
+// import { drizzle } from "drizzle-orm/neon-serverless";
+import { neon } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-http";
 import { env } from "~/env.js";
 import * as schema from "./schema";
 
-neonConfig.fetchConnectionCache = true;
+// neonConfig.fetchConnectionCache = true;
 
-const pool = new Pool({ connectionString: env.DATABASE_URL });
+// const pool = new Pool({ connectionString: env.DATABASE_URL });
 
-export const db = drizzle(pool, { schema });
+const connectionString = env.DATABASE_URL;
+const client = neon(connectionString);
+
+// export const db = drizzle(pool, { schema });
+
+export const db = drizzle(client, { schema: schema });
