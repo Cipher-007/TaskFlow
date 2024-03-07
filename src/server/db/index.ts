5857dd73bd2d8a73df92a6750a1847277f2397e2
Gaurav Deshmukh <gaurav7deshmukh@gmail.com>
2024-03-07 14:26:03 +0530

    removed db websocket connection

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,17 +1,10 @@
-// import { neonConfig, Pool } from "@neondatabase/serverless";
-// import { drizzle } from "drizzle-orm/neon-serverless";
 import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
+
 import { env } from "~/env.js";
 import * as schema from "./schema";
 
-// neonConfig.fetchConnectionCache = true;
-
-// const pool = new Pool({ connectionString: env.DATABASE_URL });
-
 const connectionString = env.DATABASE_URL;
 const client = neon(connectionString);
-
-// export const db = drizzle(pool, { schema });
 
 export const db = drizzle(client, { schema: schema });
