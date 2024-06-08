dad63b7be1f04004c712a5a668fbedc064902c68
Gaurav Deshmukh <67900669+Cipher-007@users.noreply.github.com>
2024-06-09 01:45:55 +0530

    Jwt (#8)
    * updated nextauth and removed auth/core
    
    * switch to jwt
    
    * removed session related tables
    
    * renamed

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,9 +1,7 @@
-import type { AdapterAccount } from "@auth/core/adapters";
 import { relations, sql, type InferSelectModel } from "drizzle-orm";
 import {
   boolean,
   index,
-  integer,
   pgEnum,
   pgTableCreator,
   primaryKey,
@@ -78,8 +76,6 @@ export const users = pgTable("user", {
 });
 
 export const usersRelations = relations(users, ({ one, many }) => ({
-  accounts: many(accounts),
-  sessions: many(sessions),
   organization: one(organization, {
     fields: [users.organizationId],
     references: [organization.id],
@@ -92,69 +88,6 @@ export const usersRelations = relations(users, ({ one, many }) => ({
   comment: many(comment),
   userToTeam: many(userToTeam),
 }));
-
-export const accounts = pgTable(
-  "account",
-  {
-    userId: varchar("userId", { length: 255 })
-      .notNull()
-      .references(() => users.id, { onDelete: "cascade" }),
-    type: varchar("type", { length: 255 })
-      .$type<AdapterAccount["type"]>()
-      .notNull(),
-    provider: varchar("provider", { length: 255 }).notNull(),
-    providerAccountId: varchar("providerAccountId", { length: 255 }).notNull(),
-    refresh_token: text("refresh_token"),
-    access_token: text("access_token"),
-    expires_at: integer("expires_at"),
-    token_type: varchar("token_type", { length: 255 }),
-    scope: varchar("scope", { length: 255 }),
-    id_token: text("id_token"),
-    session_state: varchar("session_state", { length: 255 }),
-  },
-  (account) => ({
-    compoundKey: primaryKey({
-      columns: [account.provider, account.providerAccountId],
-    }),
-    userIdIdx: index("account_userId_idx").on(account.userId),
-  }),
-);
-
-export const accountsRelations = relations(accounts, ({ one }) => ({
-  user: one(users, { fields: [accounts.userId], references: [users.id] }),
-}));
-
-export const sessions = pgTable(
-  "session",
-  {
-    sessionToken: varchar("sessionToken", { length: 255 })
-      .notNull()
-      .primaryKey(),
-    userId: varchar("userId", { length: 255 })
-      .notNull()
-      .references(() => users.id, { onDelete: "cascade" }),
-    expires: timestamp("expires", { mode: "date" }).notNull(),
-  },
-  (session) => ({
-    userIdIdx: index("session_userId_idx").on(session.userId),
-  }),
-);
-
-export const sessionsRelations = relations(sessions, ({ one }) => ({
-  user: one(users, { fields: [sessions.userId], references: [users.id] }),
-}));
-
-export const verificationTokens = pgTable(
-  "verificationToken",
-  {
-    identifier: varchar("identifier", { length: 255 }).notNull(),
-    token: varchar("token", { length: 255 }).notNull(),
-    expires: timestamp("expires", { mode: "date" }).notNull(),
-  },
-  (vt) => ({
-    compoundKey: primaryKey({ columns: [vt.identifier, vt.token] }),
-  }),
-);
 
 export const organization = pgTable(
   "organization",
@@ -327,7 +260,7 @@ export const comment = pgTable(
       precision: 3,
     }).$onUpdate(() => new Date()),
     projectId: uuid("project_id").notNull(),
-    userId: varchar("createdById", { length: 255 })
+    userId: varchar("created_by_id", { length: 255 })
       .notNull()
       .references(() => users.id, { onDelete: "cascade" }),
     content: text("content").notNull(),
@@ -353,7 +286,7 @@ export const permission = pgTable(
   {
     id: uuid("id").defaultRandom().primaryKey(),
     type: permissionType("type").default("NONE").notNull(),
-    userId: varchar("createdById", { length: 255 })
+    userId: varchar("created_by_id", { length: 255 })
       .notNull()
       .references(() => users.id, { onDelete: "cascade" }),
     projectId: uuid("project_id").references(() => project.id, {
@@ -400,7 +333,7 @@ export const request = pgTable(
     organizationId: uuid("organization_id")
       .notNull()
       .references(() => organization.id, { onDelete: "cascade" }),
-    userId: varchar("createdById", { length: 255 })
+    userId: varchar("created_by_id", { length: 255 })
       .notNull()
       .references(() => users.id, { onDelete: "cascade" }),
     status: requestStatus("status").default("PENDING").notNull(),
